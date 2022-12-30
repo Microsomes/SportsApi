cc3877531b7c746ebba6c322d60c87c549cf78cc
tayyab javed <tayyabdev@outlook.com>
2022-12-30 06:13:56 +0000

    init

---
+++ handlers/intro.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type HttpDoc struct {
+	Path        string `json:"path"`
+	Description string `json:"description"`
+}
+
+type Docs []*HttpDoc
+
+func AllDocs() *Docs {
+
+	docs := Docs{}
+
+	docs = append(docs, &HttpDoc{
+		Path:        "/leagues",
+		Description: "returns a list of supported leagues",
+	})
+
+	return &docs
+}
+
+func (*Docs) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	docs := AllDocs()
+	b, _ := json.Marshal(docs)
+	w.Write(b)
+}
